58dd8074acd86395ee2a647d572bc77039ee2068
angel-chen <rikugou@gmail.com>
2018-06-20 12:31:14 -0400

    Added google map

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { MapView } from 'expo';
 
 import UserLocation from './components/UserLocation';
 
@@ -14,18 +15,27 @@ export default class App extends React.Component {
   };
   render() {
     return (
-      <View style={styles.container}>
-        <UserLocation onGetLocation={this.getUserLocationHandler} />
-      </View>
+      // <View style={styles.container}>
+      //   <UserLocation onGetLocation={this.getUserLocationHandler} />
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 40.705076,
+          longitude: -74.00916,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+      // </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
