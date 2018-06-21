e73374e384e6b8021f5cb043c7cf9f4954d6bf58
Molly Seeley <mseeley89@gmail.com>
2018-06-21 15:04:47 -0400

    can get console when in perimeter

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { MapView } from 'expo';
+import Geofence from 'react-native-expo-geofence';
 
-import UserLocation from './components/UserLocation';
+var fakePoints = [
+  { key: 1, latitude: 40.705554, longitude: -74.013444, title: 'Charging Bull' },
+  { key: 2, latitude: 40.704343, longitude: -74.012981, title: 'A Starbucks' },
+  { key: 3, latitude: 40.702265, longitude: -74.011981, title: 'Retro Fitness' },
+  { key: 4, latitude: 40.703712, longitude: -74.009220, title: 'Chase Bank' },
+]
 
 export default class App extends React.Component {
   constructor() {
@@ -10,28 +15,64 @@ export default class App extends React.Component {
     this.state = {
       latitude: 0,
       longitude: 0,
+      markers: [],
+      distance: 500,
+      showCircle: true
     }
+    this.inPerimeter = this.inPerimeter.bind(this)
   }
 
 
   componentDidMount() {
     this.watchID = navigator.geolocation.watchPosition(
       position => {
-        console.log("WERTCHED POSITION", position)
         this.setState({
           latitude: position.coords.latitude,
           longitude: position.coords.longitude,
         });
-        console.log(this.state)
+        // console.log(this.state)
+        // console.warn(this.state)
       }
     );
+
+    const markers = Geofence.filterByProximity(fakePoints[0], {
+      latitude: 40.705076,
+      longitude: -74.00916,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    }, this.state.distance / 1000);
+    this.setState({ markers });
+
   }
 
   componentWillUnmount() {
     navigator.geolocation.clearWatch(this.watchID)
   }
 
+  distanceInKM(point1, point2) {
+    let lat1 = point1.latitude;
+    let lon1 = point1.longitude;
+    let lat2 = point2.latitude;
+    let lon2 = point2.longitude;
+
+    let p = 0.017453292519943295;
+    let c = Math.cos;
+    let a = 0.5 - c((lat2 - lat1) * p) / 2 +
+      c(lat1 * p) * c(lat2 * p) *
+      (1 - c((lon2 - lon1) * p)) / 2;
+
+    return 12742 * Math.asin(Math.sqrt(a));
+  }
+
+  inPerimeter(userCoords, targetCoords) {
+
+    const distance = this.distanceInKM(userCoords, targetCoords)
+
+    return distance <= this.state.distance / 1000
+  }
+
   render() {
+    const { markers } = this.state
     return (
       <MapView
         style={{ flex: 1 }}
@@ -42,7 +83,15 @@ export default class App extends React.Component {
           longitudeDelta: 0.0421,
         }}
       >
+        {
+          this.inPerimeter({
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
+          }, fakePoints[0]) &&
+          console.warn('in perimeter!!')
+        }
         <MapView.Marker
+          pinColor="#000000"
           coordinate={
             {
               latitude: this.state.latitude,
@@ -50,17 +99,28 @@ export default class App extends React.Component {
             }
           }
         />
+
+        {this.state.markers.map(marker => (
+          <MapView.Marker
+            key={marker.key}
+            coordinate={marker}
+            title={marker.title}
+            description={null}
+          />
+        ))}
+        {this.state.markers.map(marker => (
+          <MapView.Circle
+            key={marker.key}
+            center={{
+              latitude: marker.latitude, longitude: marker.longitude, latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+            radius={this.state.distance}
+            strokeColor='transparent'
+            fillColor="rgba(0, 0, 0, 0.2)" />
+        ))}
       </MapView>
 
     );
   }
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
