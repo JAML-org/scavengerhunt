7a8b190dc8b2ea44dca87effad8bea61a6abc8f6
Molly Seeley <mseeley89@gmail.com>
2018-06-22 22:01:22 -0400

    created profile component, no functionality yet

--- components/Menu.js
+++ components/Menu.js
@@ -2,11 +2,56 @@ import React from 'react';
 import { DrawerNavigator } from 'react-navigation';
 
 export const Menu = DrawerNavigator({
-
-},
-  {
-
-  }
-)
+  Profile: {
+    screen: Profile,
+    navigationOptions: {
+      drawer: {
+        label: 'Profile'
+      }
+    }
+  },
+  JoinHunt: {
+    screen: JoinHunt,
+    navigationOptions: {
+      drawer: {
+        label: 'Join Hunt'
+      }
+    }
+  },
+  ChooseHunt: {
+    screen: ChooseHunt,
+    navigationOptions: {
+      drawer: {
+        label: 'Choose Hunt'
+      }
+    }
+  },
+  MyHuntList: {
+    screen: HuntList,
+    navigationOptions: {
+      drawer: {
+        label: 'My Hunt List'
+      }
+    }
+  },
+  Friends: {
+    screen: Friends,
+    navigationOptions: {
+      drawer: {
+        label: 'Friends'
+      }
+    }
+  },
+  HowToPlay: {
+    screen: HowToPlay,
+    navigationOptions: {
+      drawer: {
+        label: 'How To Play'
+      }
+    }
+  },
+  // LogOut: {
+  // }
+})
 
 export default Menu
