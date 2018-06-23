0a282ad6b73232b71fed0dc14ebaae4e531ed8bd
angel-chen <rikugou@gmail.com>
2018-06-23 17:54:50 -0400

    Add Hunt Details and Hunt Targets page

--- router.js
+++ router.js
@@ -4,6 +4,7 @@ import UserLoginForm from './components/UserLoginForm';
 import RegForm from './components/RegForm';
 import HuntList from './components/HuntList';
 import HuntDetails from './components/HuntDetails';
+import HuntTargets from './components/HuntTargets';
 export const RootStack = createStackNavigator(
   {
     UserLogin: {
@@ -17,6 +18,9 @@ export const RootStack = createStackNavigator(
     },
     HuntDetails: {
       screen: HuntDetails,
+    },
+    HuntTargets: {
+      screen: HuntTargets,
     },
   },
   {
