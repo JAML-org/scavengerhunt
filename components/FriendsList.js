7c0e18ba910d731244d7369d86568441129e666d
Linda Morales <lindagmorales94@gmail.com>
2018-07-02 11:23:08 -0400

    Changed the navigate from HuntDetails to Map again

--- components/FriendsList.js
+++ components/FriendsList.js
@@ -7,80 +7,41 @@ import {
   ListItem,
   SearchBar,
 } from 'react-native-elements';
+import FriendCard from './FriendCard';
 
 const FriendsList = props => {
   const { search, friends, clearSearch, searchedFriend, select } = props;
   return (
-    <View>
-      <View>
-        <SearchBar
-          lightTheme
-          showLoading
-          clearIcon={true}
-          onChangeText={text => search(text)}
-          onClear={() => clearSearch()}
-          value={searchedFriend}
-          placeholder="Search Friend"
-        />
-        <Divider />
-      </View>
-      <View>
-        {friends ? (
-          <List>
-            {friends.map(friend => (
-              <ListItem
-                key={friend}
-                roundAvatar
-                title={friend.name}
-                subtitle={`Username: ${friend.username}`}
-                avatar={{ uri: friend.avatar }}
-                onPress={() => {
-                  select(friend.userId);
-                }}
-              />
-            ))}
-          </List>
-        ) : (
-          <Text h4 style={{ color: 'white' }}>
-            Add Friends
-          </Text>
-        )}
-      </View>
+    <View style={styling.flex1}>
+      <SearchBar
+        lightTheme
+        showLoading
+        clearIcon={true}
+        onChangeText={text => search(text)}
+        onClear={() => clearSearch()}
+        value={searchedFriend}
+        placeholder="Search Friend"
+      />
+      <Divider />
+      {friends ? (
+        <List>
+          {friends.map(friend => (
+            <FriendCard friend={friend} key={friend} select={select} />
+          ))}
+        </List>
+      ) : (
+        <Text h4 style={{ color: 'white' }}>
+          Add Friends
+        </Text>
+      )}
     </View>
   );
 };
 
-// const styles = StyleSheet.create({
-//   regform: {
-//     alignSelf: 'stretch',
-//   },
-//   header: {
-//     fontSize: 24,
-//     color: '#fff',
-//     paddingBottom: 10,
-//     marginBottom: 40,
-//     borderBottomColor: '#199187',
-//     borderBottomWidth: 1,
-//   },
-//   textinput: {
-//     alignSelf: 'stretch',
-//     height: 40,
-//     marginBottom: 30,
-//     color: 'black',
-//     borderBottomColor: '#f8f8f8',
-//     borderBottomWidth: 1,
-//   },
-//   button: {
-//     alignSelf: 'stretch',
-//     alignItems: 'center',
-//     padding: 20,
-//     backgroundColor: '#59cbbd',
-//     marginTop: 30,
-//   },
-//   btntext: {
-//     color: 'white',
-//     fontWeight: 'bold',
-//   },
-// });
+const styling = StyleSheet.create({
+  flex1: {
+    flex: 1,
+  },
+});
 
 export default FriendsList;
--- components/style.js
+++ components/style.js
@@ -17,6 +17,7 @@ const styles = StyleSheet.create({
   bgImage: {
     width: '100%',
     height: '100%',
+    flex: 1,
   },
   container: {
     flex: 1,
